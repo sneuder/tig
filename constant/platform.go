1f46017e6a830ecdc1870e05bf21eab869c60208
sneuder <sneuder8@gmail.com>
2025-04-30 08:14:32 -0500

    feat: add gitlab

--- constant/platform.go
+++ constant/platform.go
@@ -3,9 +3,11 @@ package constant
 const (
 	BITBUCKET = "bitbucket.org"
 	GITHUB    = "github.com"
+	GITLABD   = "gitlab.com"
 )
 
 var PlatFormOptions = map[string]string{
 	"bitbucket": BITBUCKET,
 	"github":    GITHUB,
+	"gitlab":    GITLABD,
 }
