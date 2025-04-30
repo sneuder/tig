d60826c4f1ab90c987ad62403e1ee37a4dd14f01
sneuder <sneuder8@gmail.com>
2025-04-30 08:15:06 -0500

    fix: adjust gitlab name

--- constant/platform.go
+++ constant/platform.go
@@ -3,11 +3,11 @@ package constant
 const (
 	BITBUCKET = "bitbucket.org"
 	GITHUB    = "github.com"
-	GITLABD   = "gitlab.com"
+	GITLAB    = "gitlab.com"
 )
 
 var PlatFormOptions = map[string]string{
 	"bitbucket": BITBUCKET,
 	"github":    GITHUB,
-	"gitlab":    GITLABD,
+	"gitlab":    GITLAB,
 }
